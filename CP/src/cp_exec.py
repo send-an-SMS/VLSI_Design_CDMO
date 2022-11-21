925cd527222b57d35a8fb6c1b2ca55d5a6f849b4
Simona Scala <simona.scala@hotmail.it>
2022-11-21 23:22:10 +0100

    function plot created in cp_exec.py
    I've created the function plot_solution in the file cp_exec.py. Everything works fine until the 30th instance. Need to check and fix the error. I've let the plot.py file to make some tries. Do not delete it

--- CP/src/cp_exec.py
+++ CP/src/cp_exec.py
@@ -1,12 +1,13 @@
 import argparse
 import numpy as np
+import matplotlib.pyplot as plt
 import time
 from pathlib import Path
+from matplotlib import colors
 from datetime import timedelta
 from minizinc import Solver, Instance, Model
 
-def cp_exec(first_i, last_i, rotation):
-    print(rotation)
+def cp_exec(first_i, last_i, rotation, plot):
     for i in range(first_i, last_i+1):
         ins_path = Path('../CP/instances/ins-' + str(i) + '.txt') # path of the current instance
         f = open(ins_path, 'r')
@@ -56,8 +57,6 @@ def cp_exec(first_i, last_i, rotation):
         y_coord = output.solution.y_coordinates
         h = output.solution.h
 
-        print(type(x_coord))
-
         out_path = Path("../CP/out/out-" + str(i) + f"{'_rotation' if rotation else ''}.txt")
         with open(out_path, 'w') as f:
             f.writelines(f'{w} {h}\n')
@@ -65,13 +64,66 @@ def cp_exec(first_i, last_i, rotation):
             for i in range(n):
                 f.writelines(f'{x[i]} {y[i]} {x_coord[i]} {y_coord[i]}\n')
 
-def plot_solution(w, h, x, y, x_coord, y_coord):
-    # create a matrix of blocks of int
-    np.zeros(w, h)
-
-    
+        if plot:
+            plot_solution(out_path, w, h, n)
 
 
+def plot_solution(path, w, h, n):       # path of the output file, board weight, board height, total number of circuits to place
+    board = np.zeros((w, h))
+
+    file = open(path, 'r')
+    lines = file.readlines()
+
+    for i in range(2, n + 2):     # loop on the current circuit
+        block = lines[i].split()
+        block = np.array(block, dtype = np.int32)
+
+        x = block[2]    # position of the circuit on x-axis
+        y = block[3]    # position of the circuit on y-axis
+
+        # convert the position on the axes to be coherent with the indexes of the matrix
+        # y -> row = |y - (h - 1)|
+        # x -> column (same correspondance)
+        row = abs(y - (h - 1))
+        column = x
+
+        # compute the width and height of the current circuit
+        width = block[0]
+        height = block[1]
+
+        # compute the upper-left corner position of the block to start filling the sub-matrix with its value
+        row_ulc = row - (height - 1)
+        column_ulc = column
+
+        for rows in range(row_ulc, row_ulc + height):
+            for columns in range(column_ulc, column_ulc + width):
+                board[rows][columns] = i - 2
+
+    print(board)
+
+    cmap = build_cmap(n)
+
+    plt.imshow(board, interpolation='None', cmap=cmap)
+    ax = plt.gca()
+    ax.axes.get_xaxis().set_visible(False)
+    ax.axes.get_yaxis().set_visible(False)
+    plt.show()
+
+
+def build_cmap(n):
+    colors_list = ['red','fuchsia','orange','gold','yellow','cyan','dodgerblue','blue','blueviolet','lime']
+    cmap_list = []
+    count = 0
+
+    for i in range(0, n):
+        cmap_list.append(colors_list[count])
+        if count == len(colors_list) - 1:
+            count = 0
+        else: count += 1
+
+    #print(cmap_list)
+
+    return colors.ListedColormap(cmap_list)
 
 
 if __name__ == '__main__':
@@ -79,6 +131,7 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--first', help='Number of first instance', type=int, default=1)
     parser.add_argument('-l', '--last', help='Number of last instance', type=int, default=40)
     parser.add_argument('-r', '--rotation', help='Allow rotation', action='store_true')
+    parser.add_argument('-p', '--plot', help='Plot solution', action='store_true')
     args = parser.parse_args()
 
-    cp_exec(first_i=args.first, last_i=args.last, rotation=args.rotation)
+    cp_exec(first_i=args.first, last_i=args.last, rotation=args.rotation, plot=args.plot)
--- CP/src/plot.py
+++ CP/src/plot.py
@@ -3,48 +3,82 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 
 path = "../CP/out/out-1.txt"
-
-w = 8
-h = 8
-n = 4
-
-board = np.zeros((w, h))
-
 file = open(path, 'r')
 lines = file.readlines()
 
-for i in range(2, n + 2):     # loop on the current circuit
-    block = lines[i].split()
-    block = np.array(block, dtype = np.int32)
-    print(block)
+w = int(lines[0].split()[0])
+h = int(lines[0].split()[0])
+n = int(lines[1])
 
-    x = block[2]    # position of the circuit on x-axis
-    y = block[3]    # position of the circuit on y-axis
+def plot_solution(path, w, h, n):       # path of the output file, board weight, board height, total number of circuits to place
 
-    # convert the position on the axes to be coherent with the indexes of the matrix
-    # y -> row = |y - (h - 1)|
-    # x -> column (same correspondance)
-    row = abs(y - (h - 1))
-    column = x
+    board = np.zeros((w, h))
 
-    # compute the width and height of the current circuit
-    width = block[0]
-    height = block[1]
+    file = open(path, 'r')
+    lines = file.readlines()
 
-    # compute the upper-left corner position of the block to start filling the sub-matrix with its value
-    row_ulc = row - (height - 1)
-    column_ulc = column
+    for i in range(2, n + 2):     # loop on the current circuit
+        block = lines[i].split()
+        block = np.array(block, dtype = np.int32)
+        print(block)
 
-    for rows in range(row_ulc, row_ulc + height):
-        for columns in range(column_ulc, column_ulc + width):
-            board[rows][columns] = i - 2
-            print(board[rows][columns])
+        x = block[2]    # position of the circuit on x-axis
+        y = block[3]    # position of the circuit on y-axis
 
-print(board)
+        # convert the position on the axes to be coherent with the indexes of the matrix
+        # y -> row = |y - (h - 1)|
+        # x -> column (same correspondance)
+        row = abs(y - (h - 1))
+        column = x
 
-cmap = colors.ListedColormap(['red','fuchsia','cyan','lime'])
-extent = (0, w, h, 0)     # extent is a 4-element list of scalars (left, right, bottom, top)
-_, ax = plt.subplots()
-ax.imshow(board, interpolation='None', cmap=cmap, extent=extent)
-ax.grid(color='black', linewidth=0.8, linestyle='--')
-plt.show()
+        # compute the width and height of the current circuit
+        width = block[0]
+        height = block[1]
+
+        # compute the upper-left corner position of the block to start filling the sub-matrix with its value
+        row_ulc = row - (height - 1)
+        column_ulc = column
+
+        for rows in range(row_ulc, row_ulc + height):
+            for columns in range(column_ulc, column_ulc + width):
+                board[rows][columns] = i - 2
+                print(board[rows][columns])
+
+    print(board)
+
+    plt.matshow(board)
+
+    cmap = build_cmap(n)
+
+    extent = (0, w, h, 0)     # extent is a 4-element list of scalars (left, right, bottom, top)
+    plt.imshow(board, interpolation='None', cmap=cmap, extent=extent)
+    ax = plt.gca()
+    ax.axes.xaxis.set_ticklabels([])
+    ax.axes.yaxis.set_ticklabels([])
+    plt.grid(color='black', linewidth=0.8, linestyle='--')
+    plt.show()
+
+
+def build_cmap(n):
+
+    colors_list = ['red','fuchsia','yellow','cyan','blue','lime']
+    cmap_list = []
+    count = 0
+
+    for i in range(0, n):
+        cmap_list.append(colors_list[count])
+        if count == len(colors_list) - 1:
+            count = 0
+        else: count += 1
+    
+    print(cmap_list)
+
+    return colors.ListedColormap(cmap_list)
+
+
+plot_solution(path, w, h, n)
+
+
+
+
+
