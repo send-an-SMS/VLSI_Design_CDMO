61262f89465114875dc8d1dd6f9797a09cb76f3f
Simona Scala <simona.scala@hotmail.it>
2022-11-23 14:09:12 +0100

    func plot_solution now working

--- CP/src/cp_exec.py
+++ CP/src/cp_exec.py
@@ -44,14 +44,15 @@ def cp_exec(first_i, last_i, rotation, plot):
         inst['chip_height'] = y
 
         start = time.time()
-        output = inst.solve(timeout=timedelta(seconds=301), free_search=True)   # timeout: Optional[timedelta] = None   **da documentazione**
+        output = inst.solve(timeout=timedelta(seconds=100), free_search=True)   # timeout: Optional[timedelta] = None   **da documentazione**
                                                                                 # datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0) Returns : Date
         end = time.time()
         total = end - start
-        if total > 300:
-            print(f'Instance: {i}\tAborted')
-        else:
-            print(f'Instance: {i}\tTime taken: {(end-start):.03f}s')
+        print(f'Instance: {i}\tTime taken: {(end-start):.03f}s')
+        # if total > 300:
+        #     print(f'Instance: {i}\tTime exceeded')
+        # else:
+        #     print(f'Instance: {i}\tTime taken: {(end-start):.03f}s')
 
         x_coord = output.solution.x_coordinates
         y_coord = output.solution.y_coordinates
@@ -64,12 +65,18 @@ def cp_exec(first_i, last_i, rotation, plot):
             for i in range(n):
                 f.writelines(f'{x[i]} {y[i]} {x_coord[i]} {y_coord[i]}\n')
 
+        print(w)
+        print(h)
+
         if plot:
             plot_solution(out_path, w, h, n)
 
 
 def plot_solution(path, w, h, n):       # path of the output file, board weight, board height, total number of circuits to place
-    board = np.zeros((w, h))
+    board = np.zeros((h, w))        # h stands for the height of the board and, as a numpy array, it stands for the number of rows
+                                    # w stands for the width of the board and, as a numpy array, it stands for the number of columns
+
+    print(board.shape)
 
     file = open(path, 'r')
     lines = file.readlines()
@@ -78,35 +85,22 @@ def plot_solution(path, w, h, n):       # path of the output file, board weight,
         block = lines[i].split()
         block = np.array(block, dtype = np.int32)
 
-        x = block[2]    # position of the circuit on x-axis
-        y = block[3]    # position of the circuit on y-axis
-
-        # convert the position on the axes to be coherent with the indexes of the matrix
-        # y -> row = |y - (h - 1)|
-        # x -> column (same correspondance)
-        row = abs(y - (h - 1))
-        column = x
+        column = block[2]    # position of the circuit on x-axis
+        row = block[3]       # position of the circuit on y-axis
 
         # compute the width and height of the current circuit
         width = block[0]
         height = block[1]
 
-        # compute the upper-left corner position of the block to start filling the sub-matrix with its value
-        row_ulc = row - (height - 1)
-        column_ulc = column
-
-        for rows in range(row_ulc, row_ulc + height):
-            for columns in range(column_ulc, column_ulc + width):
+        for rows in range(row, row + height):
+            for columns in range(column, column + width):
                 board[rows][columns] = i - 2
-
-    print(board)
 
     cmap = build_cmap(n)
 
     plt.imshow(board, interpolation='None', cmap=cmap)
     ax = plt.gca()
-    ax.axes.get_xaxis().set_visible(False)
-    ax.axes.get_yaxis().set_visible(False)
+    ax.invert_yaxis()
     plt.show()
 
 
--- CP/src/plot.py
+++ CP/src/plot.py
@@ -2,17 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
-path = "../CP/out/out-1.txt"
+path = "../CP/out/out-40.txt"
 file = open(path, 'r')
 lines = file.readlines()
 
-w = int(lines[0].split()[0])
-h = int(lines[0].split()[0])
+w = int(lines[0].split()[0])    
+h = int(lines[0].split()[1])
 n = int(lines[1])
 
 def plot_solution(path, w, h, n):       # path of the output file, board weight, board height, total number of circuits to place
 
-    board = np.zeros((w, h))
+    board = np.zeros((h, w))        # h stands for the height of the board and, as a numpy array, it stands for the number of rows
+                                    # w stands for the width of the board and, as a numpy array, it stands for the number of columns
+
+    print(board.shape)
 
     file = open(path, 'r')
     lines = file.readlines()
@@ -20,48 +23,34 @@ def plot_solution(path, w, h, n):       # path of the output file, board weight,
     for i in range(2, n + 2):     # loop on the current circuit
         block = lines[i].split()
         block = np.array(block, dtype = np.int32)
-        print(block)
 
-        x = block[2]    # position of the circuit on x-axis
-        y = block[3]    # position of the circuit on y-axis
-
-        # convert the position on the axes to be coherent with the indexes of the matrix
-        # y -> row = |y - (h - 1)|
-        # x -> column (same correspondance)
-        row = abs(y - (h - 1))
-        column = x
+        column = block[2]    # position of the circuit on x-axis
+        row = block[3]       # position of the circuit on y-axis
 
         # compute the width and height of the current circuit
         width = block[0]
         height = block[1]
 
-        # compute the upper-left corner position of the block to start filling the sub-matrix with its value
-        row_ulc = row - (height - 1)
-        column_ulc = column
+        print("row: ", row)
+        print("column: ", column)
+        print("row + height: ", row + height)
+        print("column + width: ", column + width)
 
-        for rows in range(row_ulc, row_ulc + height):
-            for columns in range(column_ulc, column_ulc + width):
+        for rows in range(row, row + height):
+            for columns in range(column, column + width):
                 board[rows][columns] = i - 2
-                print(board[rows][columns])
-
-    print(board)
-
-    plt.matshow(board)
 
     cmap = build_cmap(n)
 
-    extent = (0, w, h, 0)     # extent is a 4-element list of scalars (left, right, bottom, top)
-    plt.imshow(board, interpolation='None', cmap=cmap, extent=extent)
+    plt.imshow(board, interpolation='None', cmap=cmap)
     ax = plt.gca()
-    ax.axes.xaxis.set_ticklabels([])
-    ax.axes.yaxis.set_ticklabels([])
-    plt.grid(color='black', linewidth=0.8, linestyle='--')
+    ax.invert_yaxis()
     plt.show()
 
 
 def build_cmap(n):
 
-    colors_list = ['red','fuchsia','yellow','cyan','blue','lime']
+    colors_list = ['red','fuchsia','orange','gold','yellow','cyan','dodgerblue','blue','blueviolet','lime']
     cmap_list = []
     count = 0
 
@@ -70,8 +59,6 @@ def build_cmap(n):
         if count == len(colors_list) - 1:
             count = 0
         else: count += 1
-    
-    print(cmap_list)
 
     return colors.ListedColormap(cmap_list)
 
